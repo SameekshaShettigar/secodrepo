7ab7db096a4752e574b9aad6f1cf874461011bfd
Sameeksha <164968851+SameekshaShettigar@users.noreply.github.com>
2024-11-18 13:36:47 +0530

    Created HelloWorld.java
    one file uploaed

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class HelloWorld
+{
+ public static void main(String args[])
+ {
+ System.out.println("Hello World");
+ Scanner scan=new Scanner(System.in);
+ //int Ivar=10;
+ System.out.println("NEter the number:");
+ int a=scan.nextInt();
+ System.out.println("The value is "+a);
+ System.out.println("NEter the float:");
+ float b=scan.nextFloat();
+ System.out.println("The value is "+b);
+ System.out.println("NEter the double:");
+ double c=scan.nextDouble();
+ System.out.println("The value is "+c);
+ }
+ }
